8df6bea139bb5563d6ea0d896527eca3ac351688
JFSerrano21 <j.serrano@uniandes.edu.co>
2023-05-21 21:38:09 -0500

    Correccion 3

--- places/places/urls.py
+++ places/places/urls.py
@@ -5,6 +5,6 @@ from django.views.decorators.csrf import csrf_exempt
 from . import places
 
 urlpatterns = [
-    url(r'^place/', places.PlaceList, name='placeList'),
-    url(r'^place/$', csrf_exempt(places.PlaceCreate), name='placeCreate'),
+    url(r'^places/', places.PlaceList, name='placeList'),
+    url(r'^placecreate/$', csrf_exempt(places.PlaceCreate), name='placeCreate'),
 ]
